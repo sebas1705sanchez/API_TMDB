6a227c728a6c4289c9b7fbe66cd4ec5acb285231
any <any>
2024-07-03 17:00:50 -0500

    commit

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import axios from 'axios'
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(express.text());
+
 const app = express();
 
+app.get("/tmdb/:page", (req, res) => {
+    const page = req.params;
 
+    const url = axios.get(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`, {});
+    const response = url.response();
+});
+app.
 
 app.listen(PORT, () => {
     console.clear();

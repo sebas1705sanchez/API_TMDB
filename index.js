b94ae6ede09baef84553556ce861e64e498288bd
any <any>
2024-07-03 17:30:52 -0500

    Added get popular movies

--- index.js
+++ index.js
@@ -5,19 +5,35 @@ import axios from 'axios'
 dotenv.config();
 
 const PORT = process.env.PORT;
+const API_KEY = process.env.API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+const app = express();
+
+const tmdbApi = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    api_key: API_KEY,
+  },
+});
 
 app.use(express.json());
 app.use(express.text());
 
-const app = express();
 
-app.get("/tmdb/:page", (req, res) => {
-    const page = req.params;
+app.get("/tmdb/:page:language", async (req, res) => {
+    const { page, language } = req.params;
 
-    const url = axios.get(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`, {});
-    const response = url.response();
+    const response = await tmdbApi.get('/movie/popular', {
+      params: {
+        language: language,
+        page: page,
+      },
+    });
+
+    res.send(response.data);
 });
-app.
+
+
 
 app.listen(PORT, () => {
     console.clear();

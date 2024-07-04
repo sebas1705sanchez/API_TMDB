6540491c363ddc253d561b2443f61f14d906dcad
FrankiNarvaez <frannki03@gmail.com>
2024-07-04 05:34:49 -0500

    Added endpoint to search people, movies and TV shows to ID

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from 'express'
+import express, { response } from 'express'
 import dotenv from 'dotenv'
-import axios from 'axios'
+import axios from 'axios' 
 
 dotenv.config();
 
@@ -9,7 +9,7 @@ const API_KEY = process.env.API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 const app = express();
 
-const tmdbApi = axios.create({
+const TMDB_API = axios.create({
   baseURL: BASE_URL,
   params: {
     api_key: API_KEY,
@@ -19,21 +19,69 @@ const tmdbApi = axios.create({
 app.use(express.json());
 app.use(express.text());
 
+app.get("/tmdb", async (req, res) => {
+    const { page, language } = req.query;
 
-app.get("/tmdb/:page:language", async (req, res) => {
-    const { page, language } = req.params;
-
-    const response = await tmdbApi.get('/movie/popular', {
+    const response = await TMDB_API.get('/movie/popular', {
       params: {
-        language: language,
         page: page,
+        language: language,
       },
     });
 
-    res.send(response.data);
+    return res.send(response.data);
 });
 
+app.get("/api/movies/:id", async (req, res) => {
+  const { id } = req.params;
+  const { language } = req.query;
 
+  try {
+    const response = await TMDB_API.get(`/movie/${id}`, {
+      params: {
+        language: language
+      }
+    });
+
+    return res.send(response.data);
+  } catch (error) {
+    return res.status(404).send();
+  }
+})
+
+app.get("/api/tv/:id", async (req, res) => {
+  const { id } = req.params;
+  const { language } = req.query;
+
+  try {
+    const response = await TMDB_API.get(`/tv/${id}`, {
+      params: {
+        language: language
+      }
+    });
+
+    return res.send(response.data);
+  } catch (error) {
+    return res.status(404).send();
+  }
+})
+
+app.get("/api/people/:id", async (req, res) => {
+  const { id } = req.params;
+  const { language } = req.query;
+
+  try {
+    const response = await TMDB_API.get(`/person/${id}`, {
+      params: {
+        language: language
+      }
+    });
+
+    return res.send(response.data);
+  } catch (error) {
+    return res.status(404).send();
+  }
+})
 
 app.listen(PORT, () => {
     console.clear();
